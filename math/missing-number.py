862c7d180dbfd766bc28e5ebccd511165f174b4f
Jack <jack.anyone@gmail.com>
2021-04-20 22:47:35 -0400

    solve missing number

---
+++ math/missing-number.py
@@ -0,0 +1,55 @@
+"""
+Link: https://leetcode.com/problems/missing-number/
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range 
+that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number 
+in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in 
+the range since it does not appear in nums.
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+"""
+
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        """
+        The idea is that the sum of the n distinct numbers should be (n+1)(n)/2, so we could substact
+        all numbers in the array to get the missing number
+        """
+        """
+        Time complexity: O(n)
+        Space complexity: O(1)
+        """
+
+        n = len(nums)
+        sum = 0
+        for i in nums:
+            sum += i 
+
+        return n*(n+1)/2 - sum
+
+if __name__ == '__main__':
+    assert Solution().missingNumber([3,0,1]) == 2
+    assert Solution().missingNumber([0,1]) == 2
+    assert Solution().missingNumber([9,6,4,2,3,5,7,0,1]) == 8
+    assert Solution().missingNumber([0]) == 1
