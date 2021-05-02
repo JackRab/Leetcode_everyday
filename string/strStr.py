996e3f0f0407f03f2f60285566fea9b630ea75ad
Jack Yi <jack.anyone@gmail.com>
2021-05-02 11:18:46 -0400

    optimize strStr to make it more clear

--- string/strStr.py
+++ string/strStr.py
@@ -6,54 +6,53 @@ Implement strStr().
 Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 Example 1:
-
 Input: haystack = "hello", needle = "ll"
 Output: 2
 
 Example 2:
-
 Input: haystack = "aaaaa", needle = "bba"
 Output: -1
-Example 3:
 
+Example 3:
 Input: haystack = "", needle = ""
 Output: 0
 
 Constraints:
-
 0 <= haystack.length, needle.length <= 5 * 104
 haystack and needle consist of only lower-case English characters.
 """
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        """
+        Return the index of the first occurrence of needle in haystack
 
-def strStr(haystack: str, needle: str) -> int:
-    """
-    Return the index of the first occurrence of needle in haystack
+        Parameters
+        ----------
+        haystack : str
+        needle : str
 
-    Parameters
-    ----------
-    haystack : str
-    needle : str
+        Returns
+        -------
+        int: the index of the first occurrence of needle in haystack; 
+             -1 if not found, 0 if needle is empty
+        """
+        """
+        Time complexity: O(N) since we are iterating haystack
+        Space complexity: O(1)
+        """
+        index = 0
+        while index <= len(haystack) - len(needle):
+            if haystack[index:index+len(needle)] == needle:
+                break
+            else:
+                index += 1
+        
+        if index > len(haystack) - len(needle):
+            return -1
 
-    Returns
-    -------
-    int: the index of the first occurrence of needle in haystack; 
-         -1 if not found, 0 if needle is empty
-    """
-    """
-    Time complexity: O(N) since we are iterating haystack
-    Space complexity: O(1)
-    """
-    if needle == "":
-        return 0
+        return index
 
-    if len(haystack) == 0:
-        return -1
-
-    for i, s in enumerate(haystack):
-        if len(haystack) - i < len(needle):
-            break
-
-        if needle == haystack[i:i+len(needle)]:
-            return i
-
-    return -1
+if __name__ == '__main__':
+    assert Solution().strStr('hello', 'll') == 2
+    assert Solution().strStr('aaaaa', 'bba') == -1
+    assert Solution().strStr('', '') == 0
