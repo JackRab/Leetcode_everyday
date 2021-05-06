a12167fd99a2f5d17a2fd72d661c2ed414c5f7ec
Jack Yi <jack.anyone@gmail.com>
2021-05-05 23:50:11 -0400

    solve consecutive numbers

---
+++ sql/consecutive-numbers.sql
@@ -0,0 +1,47 @@
+/*
+Link: https://leetcode.com/problems/consecutive-numbers/
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+ 
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+ 
+
+Logs table:
++----+-----+
+| Id | Num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+
+Result table:
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+1 is the only number that appears consecutively for at least three times.
+*/
+
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs AS l1, Logs AS l2, Logs AS l3
+WHERE l1.Id = l2.Id - 1 AND l2.Id = l3.Id - 1
+      AND l1.num = l2.num AND l2.num = l3.num;
