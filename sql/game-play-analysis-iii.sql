9cacb3067b74e8c43b0a556e9cada9cb6bb99628
Jack Yi <jack.anyone@gmail.com>
2021-05-29 21:17:47 -0400

    add games played so far

---
+++ sql/game-play-analysis-iii.sql
@@ -0,0 +1,21 @@
+/*
+Link: https://leetcode.com/problems/game-play-analysis-iii/
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some game.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
+ 
+
+Write an SQL query that reports for each player and date, how many games played so far by the player. That is, the total number of games played by the player until that date.
+*/
+SELECT player_id, event_date, SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity;
