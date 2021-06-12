a1d317434d1c976d5a6610a043a2ca2d88f8b6d2
Jack Yi <jack.anyone@gmail.com>
2021-06-12 16:04:24 -0400

    countries you can invest

---
+++ sql/countries-you-can-safely-invest-in.sql
@@ -0,0 +1,48 @@
+/*
+Link: https://leetcode.com/problems/countries-you-can-safely-invest-in/
+
+Table Person:
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| id             | int     |
+| name           | varchar |
+| phone_number   | varchar |
++----------------+---------+
+id is the primary key for this table.
+Each row of this table contains the name of a person and their phone number.
+Phone number will be in the form 'xxx-yyyyyyy' where xxx is the country code (3 characters) and yyyyyyy is the phone number (7 characters) where x and y are digits. Both can contain leading zeros.
+ 
+Table Country:
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| name           | varchar |
+| country_code   | varchar |
++----------------+---------+
+country_code is the primary key for this table.
+Each row of this table contains the country name and its code. country_code will be in the form 'xxx' where x is digits.
+ 
+Table Calls:
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| caller_id   | int  |
+| callee_id   | int  |
+| duration    | int  |
++-------------+------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the caller id, callee id and the duration of the call in minutes. caller_id != callee_id
+
+A telecommunications company wants to invest in new countries. The company intends to invest in the countries where the average call duration of the calls in this country is strictly greater than the global average call duration.
+
+Write an SQL query to find the countries where this company can invest.
+*/
+SELECT co.name AS country
+FROM Person AS p
+JOIN Calls AS c
+ON p.id IN (c.caller_id, c.callee_id)
+JOIN Country AS co
+ON SUBSTR(p.phone_number, 1, 3) = co.country_code
+GROUP BY country
+HAVING AVG(duration) > (SELECT AVG(duration) FROM Calls);
